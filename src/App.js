71a14cba706647fa7ef02f131bd8e4aa8b5fd296
= <=>
2023-07-01 13:49:57 -0300

    add main section components in home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import PageDisplay from "./pages/PageDisplay";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
+      <PageDisplay  />
     </div>
   );
 }
---
+++ src/components/Popular.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function Popular() {
+
+    const [popularRecipes, setPopularRecipes] = useState([]);
+
+    useEffect(() => {
+        getPopularRecipes();
+    }, []);
+
+    const getPopularRecipes = async () => {
+        fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+            .then(response => response.json())
+            .then(response => setPopularRecipes(response.recipes));
+    }
+    
+    return (
+        <div>
+            {
+                popularRecipes ? (
+                    popularRecipes.map(recipe => (
+                        <p>{recipe.title}</p>
+                    ))
+                ) : (
+                    <p>Loading...</p>
+                )
+            }
+        </div>
+    )
+}
+
+export default Popular
